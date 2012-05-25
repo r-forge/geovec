c60d8649396dcd612862da9c6e2ff3ccb6fcd119
rhijmans (Robert Hijmans) <r.hijmans@gmail.com>
2012-05-25 23:13:09 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/geovec@11 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/geovec/R/merge.R
+++ pkg/geovec/R/merge.R
@@ -11,19 +11,27 @@ function(x, y, by=intersect(names(x), names(y)), by.x=by, by.y=by, all.x=TRUE, s
 	}
 	d <- x@data
 	d$donotusethisvariablename976 <- 1:nrow(d)
-
+	
 	y <- unique(y)
+	i <- apply(y[, by.y, drop=FALSE], 1, paste) %in% apply(x@data[, by.x, drop=FALSE], 1, paste)
+	y <- y[i, ,drop=FALSE]
+	if (isTRUE(any(table(y[, by.y]) > 1))) {
+		stop("'y' has multiple records for one or more 'by.y' key(s)")
+	}
+	
 	if (!all.x) {
 		y$donotusethisvariablename679 <- 1
 	}
-	d <- merge(d, y, by=by, by.x=by.x, by.y=by.y, suffixes=suffixes, incomprables=incomprable, all.x=TRUE, all.y=FALSE)
+	
+	d <- merge(d, y, by=by, by.x=by.x, by.y=by.y, suffixes=suffixes, incomparables=incomparables, all.x=TRUE, all.y=FALSE)
 	d <- d[order(d$donotusethisvariablename976), ]
-	d <- d[, -which(colnames(d)=='donotusethisvariablename976'), drop=FALSE]
+	d$donotusethisvariablename976 <- NULL
 	rownames(d) <- row.names(x)
 	x@data <- d
+
 	if (! all.x ) {
 		x <- x[!is.na(x@data$donotusethisvariablename679),  ,drop=FALSE] 
-		x@data <- x@data[ , -which(colnames(x@data)=='donotusethisvariablename679'), drop=FALSE]
+		x@data$donotusethisvariablename679 <- NULL
 	}
 	x
 } 
